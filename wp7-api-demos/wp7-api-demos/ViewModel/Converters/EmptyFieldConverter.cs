daa3554f30d893b4acb048ef3227a539c049b6e2
Landowski Tomasz <landowskitomasz@gmail.com>
2012-08-01 13:52:11 +0200

    Permissions

---
+++ wp7-api-demos/wp7-api-demos/ViewModel/Converters/EmptyFieldConverter.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Windows.Data;
+
+namespace wp7_api_demos.ViewModel.Converters
+{
+    public class EmptyFieldConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            String stringValue = value as String;
+            if(String.IsNullOrEmpty(stringValue))
+                return "#############";
+            else 
+                return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
