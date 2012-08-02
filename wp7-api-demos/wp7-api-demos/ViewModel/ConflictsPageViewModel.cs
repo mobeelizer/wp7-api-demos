1123b1cdfa533cb7f44a5eace3dd30c716bc5643
Landowski Tomasz <landowskitomasz@gmail.com>
2012-08-02 13:28:26 +0200

    Conflicts

---
+++ wp7-api-demos/wp7-api-demos/ViewModel/ConflictsPageViewModel.cs
@@ -0,0 +1,211 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+using wp7_api_demos.Model.MobeelizerModels;
+using System.Collections.ObjectModel;
+using wp7_api_demos.Model;
+using Com.Mobeelizer.Mobile.Wp7.Api;
+using Com.Mobeelizer.Mobile.Wp7;
+using System.Linq;
+using System.Collections.Generic;
+
+
+namespace wp7_api_demos.ViewModel
+{
+    public class ConflictsPageViewModel : ViewModelBase
+    {
+        private ObservableCollection<conflictsEntity> entities;
+
+        public ObservableCollection<conflictsEntity> Entities
+        {
+            get
+            {
+                return this.entities;
+            }
+
+            set
+            {
+                this.entities = value;
+                this.RaisePropertyChanged("Entities");
+            }
+        }
+
+        public ConflictsPageViewModel(INavigationService navigationService, int sessionCode)
+            : base(navigationService)
+        {
+            this.Entities = new ObservableCollection<conflictsEntity>();
+            this.RefreshEntitiesList();
+            this.SessionCode = sessionCode;
+        }
+
+        private conflictsEntity selectedEntity;
+
+        public conflictsEntity SelectedEntity
+        {
+            get
+            {
+                return selectedEntity;
+            }
+
+            set
+            {
+                if (this.selectedEntity != value)
+                {
+                    if (value != null)
+                    {
+                        this.selectedEntity = value;
+                        this.navigationService.Navigate(new Uri(String.Format("/View/SelectScorePage.xaml?guid={0}", this.selectedEntity.guid), UriKind.Relative));
+                    }
+                }
+            }
+        }
+
+        public int SessionCode { get; private set; }
+
+        public ICommand AddCommand
+        {
+            get
+            {
+                return new DelegateCommand(this.OnAdd);
+            }
+        }
+
+        public ICommand SyncCommand
+        {
+            get
+            {
+                return new DelegateCommand(this.OnSync);
+            }
+        }
+
+        public ICommand UserSwitchedCommand
+        {
+            get
+            {
+                return new DelegateCommand(UserSwitched);
+            }
+        }
+
+        public ICommand SwitchingUserCommand
+        {
+            get
+            {
+                return new DelegateCommand(SwitchingUser);
+            }
+        }
+
+        private void UserSwitched(object arg)
+        {
+            MobeelizerLoginStatus status = (MobeelizerLoginStatus)arg;
+            switch (status)
+            {
+                case MobeelizerLoginStatus.OK:
+                    this.IsBusy = false;
+                    Deployment.Current.Dispatcher.BeginInvoke(new Action(() =>
+                    {
+                        this.RefreshEntitiesList();
+                    }));
+                    break;
+                case MobeelizerLoginStatus.MISSING_CONNECTION_FAILURE:
+                    navigationService.ShowMessage(Resources.Errors.e_title, Resources.Errors.e_missingConnection);
+                    break;
+                default:
+                case MobeelizerLoginStatus.AUTHENTICATION_FAILURE:
+                case MobeelizerLoginStatus.CONNECTION_FAILURE:
+                case MobeelizerLoginStatus.OTHER_FAILURE:
+                    navigationService.ShowMessage(Resources.Errors.e_title, Resources.Errors.e_cannotConnectToSession);
+                    break;
+            }
+        }
+
+        private void SwitchingUser(object arg)
+        {
+            this.BusyMessage = "Logging in";
+            this.IsBusy = true;
+        }
+
+        private void OnAdd(object param)
+        {
+            Movie movie = DataUtil.GetRandomMovie();
+            conflictsEntity entity = new conflictsEntity();
+            entity.title = movie.Title;
+            entity.score = movie.Rating;
+            using (var transaction = Mobeelizer.GetDatabase().BeginTransaction())
+            {
+                transaction.GetModels<conflictsEntity>().InsertOnSubmit(entity);
+                transaction.Commit();
+            }
+
+            this.Entities.Add(entity);
+        }
+
+        private void OnSync(object param)
+        {
+            this.BusyMessage = "Synchronizing";
+            this.IsBusy = true;
+            Mobeelizer.Sync((result) =>
+            {
+                this.IsBusy = false;
+                switch (result.GetSyncStatus())
+                {
+                    case MobeelizerSyncStatus.FINISHED_WITH_SUCCESS:
+                        Deployment.Current.Dispatcher.BeginInvoke(new Action(() =>
+                        {
+                            this.RefreshEntitiesList();
+                        }));
+                        break;
+                    case MobeelizerSyncStatus.FINISHED_WITH_FAILURE:
+                        navigationService.ShowMessage(Resources.Errors.e_title, Resources.Errors.e_syncFailed);
+                        break;
+                    case MobeelizerSyncStatus.NONE:
+                        navigationService.ShowMessage(Resources.Errors.e_title, Resources.Errors.e_syncDisabled);
+                        break;
+                }
+            });
+        }
+
+        private void RefreshEntitiesList()
+        {
+            bool inConflict = false;
+            ObservableCollection<conflictsEntity> entities = new ObservableCollection<conflictsEntity>();
+            var database = Mobeelizer.GetDatabase();
+            using (var transaction = database.BeginTransaction())
+            {
+                var query = from conflictsEntity entity in transaction.GetModels<conflictsEntity>() select entity;
+                foreach (var entity in query)
+                {
+                    if (entity.Conflicted)
+                    {
+                        inConflict = true;
+                    }
+
+                    entities.Add(entity);
+                }
+            }
+            
+            if (IsWarningVisable != inConflict)
+            {
+                IsWarningVisable = inConflict;
+            }
+
+            this.Entities.Clear();
+            this.Entities = entities;
+        }
+
+        private bool warningVisable;
+
+        public bool IsWarningVisable
+        {
+            get
+            {
+                return this.warningVisable;
+            }
+
+            private set
+            {
+                this.warningVisable = value;
+                this.RaisePropertyChanged("IsWarningVisable");
+            }
+        }
+    }
+}
---
+++ wp7-api-demos/wp7-api-demos/ViewModel/SelectScorePageViewModel.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Collections.ObjectModel;
+using Com.Mobeelizer.Mobile.Wp7;
+using wp7_api_demos.Model.MobeelizerModels;
+using System.Linq;
+
+namespace wp7_api_demos.ViewModel
+{
+    public class SelectScorePageViewModel : ViewModelBase
+    {
+        private String modelGuid;
+
+        public ObservableCollection<int> Options { get; private set; }
+
+        public SelectScorePageViewModel(INavigationService navigationService, String modelGuid)
+            : base(navigationService)
+        {
+            this.modelGuid = modelGuid;
+            this.Options = new ObservableCollection<int>();
+            for(int i = 1; i<6; ++i)
+            {
+                this.Options.Add(i);
+            }
+        }
+
+        public int SelectedOption
+        {
+            get
+            {
+                return -1;
+            }
+
+            set
+            {
+                using (var transaction = Mobeelizer.GetDatabase().BeginTransaction())
+                {
+                    var query = from conflictsEntity e in transaction.GetModels<conflictsEntity>() where e.guid == modelGuid select e;
+                    conflictsEntity entity = query.Single();
+                    entity.score = value;
+                    transaction.Commit();
+                }
+                this.navigationService.GoBack();
+            }
+        }
+    }
+}
